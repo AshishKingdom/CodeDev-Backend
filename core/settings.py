2f1fb1919e7441e49dcaaf3402aababd35b0a8ea
Ashish Kushwaha <ashishkushwahacb@gmail.com>
2023-09-02 23:46:01 +0530

    removed unused env vars

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,5 @@ class Settings:
     DB_URL = os.getenv("DB_URL")
 
     JWT_ACCESS_KEY = os.getenv("JWT_ACCESS_KEY")
-    JWT_REFRESH_KEY = os.getenv("JWT_REFRESH_KEY")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-    REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
-    ORIGINS = os.getenv("ORIGINS").split(',')
